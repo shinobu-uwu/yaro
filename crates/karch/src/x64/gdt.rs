be913dc0635699991fc797b9e598d4ee32cb072f
Matheus Filipe dos Santos Reinert <matheus.reinert@protonmail.com>
2026-09-23 17:34:10 -0300

    starting gdt :)

--- crates/karch/src/x64/gdt.rs
+++ crates/karch/src/x64/gdt.rs
@@ -1,1 +1,22 @@
-pub struct Gdt {}
+#[derive(Debug, Clone)]
+pub struct GlobalDescriptorTable<const N: usize = 8> {
+    entries: [Entry; N],
+    len: usize,
+}
+
+#[repr(C, packed)]
+#[derive(Debug, Clone, Copy)]
+pub struct Entry {
+    pub limitl: u16,
+    pub offsetl: u16,
+    pub offsetm: u8,
+    pub access: u8,
+    pub flags_limith: u8,
+    pub offseth: u8,
+}
+
+#[derive(Debug, Clone, Copy)]
+pub enum Descriptor {
+    UserSegment(u64),
+    SystemSegment(u64, u64),
+}
